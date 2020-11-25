c1c282df258247c52c00c5c7316e8c4033ce569b
Smaranika Mishra <romioranjans@gmail.com>
2020-11-25 10:43:05 +0530

    multithreading

---
+++ AssignmentsOrHandson/Class2_java8/ThreadDemo.java
@@ -0,0 +1,14 @@
+package Class2_java8;
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+
+		Counter counter=new Counter();
+		Thread t1=new Thread(counter);
+		Thread t2=new Thread(counter);
+		t1.start();
+		t2.start();
+	}
+
+}

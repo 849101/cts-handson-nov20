b1157c701b4c2d9a4d3c5483858786db1760d24f
Smaranika Mishra <romioranjans@gmail.com>
2020-11-25 10:59:04 +0530

    2nd class

--- AssignmentsOrHandson/Class2_java8/Counter1.java
+++ AssignmentsOrHandson/Class2_java8/Counter1.java
@@ -1,11 +1,11 @@
 package Class2_java8;
 
-public class Counter implements Runnable {
-
+public class Counter1 implements Runnable {
 	@Override
-	public synchronized void run() {
+	public  void run() {
 		for(int i=1;i<=100;i++) {
 			System.out.println("Counter :"+i);
 		}
 	}
+
 }
--- AssignmentsOrHandson/Class2_java8/ThreadDemo1.java
+++ AssignmentsOrHandson/Class2_java8/ThreadDemo1.java
@@ -1,16 +1,15 @@
 package Class2_java8;
 
-public class ThreadDemo {
+public class ThreadDemo1 {
 
 	public static void main(String[] args) {
 
-		Counter counter=new Counter();
-		Thread t1=new Thread(counter,"A");
-		Thread t2=new Thread(counter,"B");
+		Counter1 counter=new Counter1();
+		Thread t1=new Thread(counter,"1");
+		Thread t2=new Thread(counter,"2");
 		t1.start();
 		t2.start();
 		System.out.println("End of the program");
-
 	}
 
 }

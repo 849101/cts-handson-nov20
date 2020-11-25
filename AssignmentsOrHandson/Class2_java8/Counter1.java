cc86872c344e0e770dd166664814b8cbf8301a41
849101 <74948185+849101@users.noreply.github.com>
2020-11-25 11:01:50 +0530

    Update Counter1.java

--- AssignmentsOrHandson/Class2_java8/Counter1.java
+++ AssignmentsOrHandson/Class2_java8/Counter1.java
@@ -2,7 +2,7 @@ package Class2_java8;
 
 public class Counter1 implements Runnable {
 	@Override
-	public  void run() {
+	public synchronized void run() {
 		for(int i=1;i<=100;i++) {
 			System.out.println("Counter :"+i);
 		}
